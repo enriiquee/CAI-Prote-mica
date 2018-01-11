e5a4c6a16568abc96bc80af9e21f93d892dff98c
Enrique <enrique.perez2@um.es>
2018-01-11 18:09:39 +0100

    Something edited

--- Proteome_Discoverer_Filter/Filter_Proteome_Discoverer.R
+++ Proteome_Discoverer_Filter/Filter_Proteome_Discoverer.R
@@ -121,7 +121,7 @@ if (length(files_glob_peptides) != length(files_glob_proteins)) {
     lst <- list(df1, df2)
     
     
-    if(length(df1)!=length(df2)){
+
     #Create a list where we can test similars
     alltests <- unique(trimws(unlist(lst, recursive = TRUE)))
     df_final <- as.data.frame(
@@ -136,7 +136,7 @@ if (length(files_glob_peptides) != length(files_glob_proteins)) {
       #print(i)
       duplicate4 <- duplicate4[!duplicate4$Accession == as.character(i), ]
     }
-    }
+    
     # duplicate4$`Peptides(95%)` <- duplicate3$numdup
     # duplicate4$Accession <- gsub("\\;.*","", duplicate4$Accession)
     
@@ -439,7 +439,7 @@ if (length(files_glob_peptides) != length(files_glob_proteins)) {
         
         #Merge data. 
         Comunes_ALL<-rbind(test1, test2, test3, test4, test5, test6, test7, test8, 
-                           test9, test10, test11, test12, test13, test14, test15,test16,test17)
+                           test9, test10, test11, test12, test13, test14, test15,test16,test17, test18, test19, test20, test21, test22, test23,test24,test25,test26,test27,test28,test29,test30,test31)
         
         Comunes5a5 <- test1
         Comunes4a4 <- rbind(test2,test3,test4,test5,test6)
@@ -473,7 +473,7 @@ if (length(files_glob_peptides) != length(files_glob_proteins)) {
         
         #Export data frame to table.
         
-        WriteXLS(dfList, ExcelFileName = "MulticonsensoA_comunesA3.xls", names(dfList))
+        WriteXLS(dfList, ExcelFileName = "MulticonsensoB_5.xls", names(dfList))
       	
       }else if((length(grep(x = colnames(Proteins_PP4), pattern = "Ions.Score.*4.Mascot"))==2)){
 
